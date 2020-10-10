2b682d305f3b6938b879f41e5f2d3ccce2624d4c
mragicl <mragicl@protonmail.com>
2020-10-10 15:04:08 +0000

    add build option for arm64

--- rocketpool-cli/build.sh
+++ rocketpool-cli/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 GOOS=linux GOARCH=amd64 go build -o rocketpool-cli-linux-amd64 rocketpool-cli.go
+GOOS=linux GOARCH=arm64 go build -o rocketpool-cli-linux-arm64 rocketpool-cli.go
 GOOS=darwin GOARCH=amd64 go build -o rocketpool-cli-darwin-amd64 rocketpool-cli.go
 GOOS=windows GOARCH=amd64 go build -o rocketpool-cli-windows-amd64.exe rocketpool-cli.go
